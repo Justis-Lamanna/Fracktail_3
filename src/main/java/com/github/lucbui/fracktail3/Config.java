bb02f98d9a72bbdaf54f44d37005ef60bf9cc2fa
Justis Lamanna <lamanna.justis@gmail.com>
2021-05-07 12:42:57 -0500

    Commenting out offending code for easier debugging?

--- src/main/java/com/github/lucbui/fracktail3/Config.java
+++ src/main/java/com/github/lucbui/fracktail3/Config.java
@@ -1,36 +1,24 @@
 package com.github.lucbui.fracktail3;
 
-import com.github.lucbui.fracktail3.discord.config.DiscordConfiguration;
-import com.github.lucbui.fracktail3.discord.context.ReplyStyle;
-import com.github.lucbui.fracktail3.discord.platform.DiscordPlatform;
-import com.github.lucbui.fracktail3.magic.platform.Platform;
-import com.github.lucbui.fracktail3.magic.platform.context.BasicParameterParser;
-import discord4j.core.event.ReactiveEventAdapter;
-import discord4j.core.object.presence.Activity;
-import discord4j.core.object.presence.Presence;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import java.util.List;
 
 @Configuration
 @EnableScheduling
 public class Config {
-    @Bean
-    public Platform discord(@Value("${discord.token}") String token, List<ReactiveEventAdapter> hooks) {
-        DiscordConfiguration configuration = DiscordConfiguration.builder()
-                .token(token)
-                .prefix("!")
-                .initialPresence(Presence.doNotDisturb(Activity.watching("you be such a cutie")))
-                .replyStyle(ReplyStyle.PLAIN)
-                .hooks(hooks)
-                .build();
-
-        return new DiscordPlatform(
-                configuration,
-                new BasicParameterParser()
-        );
-    }
+//    @Bean
+//    public Platform discord(@Value("${discord.token}") String token, List<ReactiveEventAdapter> hooks) {
+//        DiscordConfiguration configuration = DiscordConfiguration.builder()
+//                .token(token)
+//                .prefix("!")
+//                .initialPresence(Presence.doNotDisturb(Activity.watching("you be such a cutie")))
+//                .replyStyle(ReplyStyle.PLAIN)
+//                .hooks(hooks)
+//                .build();
+//
+//        return new DiscordPlatform(
+//                configuration,
+//                new BasicParameterParser()
+//        );
+//    }
 }
