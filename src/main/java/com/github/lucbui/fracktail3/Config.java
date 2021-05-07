10bf1314e96a7ef325058e1d07c86194632e428f
Justis Lamanna <lamanna.justis@gmail.com>
2021-05-07 12:47:19 -0500

    Commenting out offending code for easier debugging pt 2?

--- src/main/java/com/github/lucbui/fracktail3/Config.java
+++ src/main/java/com/github/lucbui/fracktail3/Config.java
@@ -1,7 +1,11 @@
 package com.github.lucbui.fracktail3;
 
+import com.github.lucbui.fracktail3.magic.Bot;
+import com.github.lucbui.fracktail3.magic.platform.*;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableScheduling
@@ -21,4 +25,34 @@ public class Config {
 //                new BasicParameterParser()
 //        );
 //    }
+
+    @Bean
+    public Platform empty() {
+        return new Platform() {
+            @Override
+            public Mono<Boolean> start(Bot bot) {
+                return Mono.never();
+            }
+
+            @Override
+            public Mono<Boolean> stop(Bot bot) {
+                return Mono.never();
+            }
+
+            @Override
+            public Mono<Person> getPerson(String id) {
+                return Mono.just(NonePerson.INSTANCE);
+            }
+
+            @Override
+            public Mono<Place> getPlace(String id) {
+                return Mono.just(NonePlace.INSTANCE);
+            }
+
+            @Override
+            public String getId() {
+                return "empty";
+            }
+        };
+    }
 }
